40ca1be89be77d2cb5039c00a2c3a254a22ab4cd
kristianrasmussen1 <kr94@live.dk>
2016-04-08 10:16:10 +0200

    Test again

--- ProjectPlanner/src/pp/app/HelloApp.java
+++ ProjectPlanner/src/pp/app/HelloApp.java
@@ -4,5 +4,6 @@ public class HelloApp {
 	public static void main(String[] args) {
 		System.out.println("Hello ProjectPlanner!");
 		System.out.println("Hello again!");
+		System.out.println("Hello again again!");
 	}
 }
