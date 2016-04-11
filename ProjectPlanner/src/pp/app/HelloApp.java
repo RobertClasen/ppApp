ec2e0fa615a86a832bc777b93a58e21970b7620f
marcoillemann <s153466@student.dtu.dk>
2016-04-11 17:09:24 +0200

    Merged Hello

--- ProjectPlanner/src/pp/app/HelloApp.java
+++ ProjectPlanner/src/pp/app/HelloApp.java
@@ -2,9 +2,24 @@ package pp.app;
 
 public class HelloApp {
 	public static void main(String[] args) {
-		System.out.println("Hello ProjectPlanner!");
-		System.out.println("Hello again!");
-		System.out.println("Hello again again!");
-		System.out.println("Hello again again again!");
+//		System.out.println("Hello ProjectPlanner!");
+//		System.out.println("Hello again!");
+//		System.out.println("Hello again again!");
+//		System.out.println("Hello again again again!");
+		
+		System.out.println(again());
+		
+	}
+	
+	public static String again() {
+		System.out.print("Hello ");
+	int i = 0; 
+	while(i<50) {
+	System.out.print("again ");	
+	i++;
+	}
+	return "again";
 	}
 }
+
+
