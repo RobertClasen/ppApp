92766ecef1d779258c70973178ee7d1247c463ff
RobertClasen <s143321@dtu.dk>
2016-04-25 18:12:56 +0200

    Tests og implementering af user navne. Tests og implementering af generering af userID.

--- ProjectPlanner/src/pp/app/PpApp.java
+++ ProjectPlanner/src/pp/app/PpApp.java
@@ -15,15 +15,54 @@ public class PpApp {
 		return isLoggedIn;
 	}
 
+	
 	public void registerUser(User u) throws RegistrationException {
-		
-		for (User user : users) {
-			if(u.getUserId().equals(user.getUserId())){
-				throw new RegistrationException("UserId already in use", "Register user");
-			}
-		}
-		
+		String userId = validUserId(u);
+		u.setUserId(userId);
 		users.add(u);
+		
+//		for (User user : users) {
+//			if(u.getUserId().equals(user.getUserId())){
+//				throw new RegistrationException("UserId already in use", "Register user");
+//			}
+//		}
+		
+	}
+
+	/*
+	 * Generates a userID based on the users names. If it's not unique: generate new userID.
+	 */
+	private String validUserId(User u) {
+		String userId = "";
+		boolean uniqueIdFound = false;
+		int i = 0;
+		int j = 2;
+		
+		while(!uniqueIdFound){
+			userId = generateUserId(u, i, j);
+			i++;
+			j++;
+			if (!checkForDuplicates(userId)){
+				uniqueIdFound = true;
+			}
+		}		
+		
+		return userId;
+		//Checks for dublicate userIDs. If such is found, generates a new userID and runs the loop again.
+		
+	}
+	
+	private String generateUserId(User u, int i, int j) {
+		return (u.getFirstname().substring(0,2) + u.getLastname().substring(i,j)).toLowerCase();
+	}
+
+	private boolean checkForDuplicates(String userId){
+		for (User user : users) {
+				if (user.getUserId().equals(userId)) {
+					return  true;
+				}
+			}
+		return false;
 	}
 
 	public List<User> getUsers() {
--- ProjectPlanner/test/pp/app/TestUsers.java
+++ ProjectPlanner/test/pp/app/TestUsers.java
@@ -8,46 +8,60 @@ import org.junit.Test;
 public class TestUsers {
 	
 	PpApp ppApp;
-	User u; 
+	User user1; 
 	
 	@Before
 	public void setUp() {
 		ppApp = new PpApp();
-		u = new User("Andreas", "Ustrup");
+		user1 = new User("John", "Nielsen");
 	}
 	
 	@Test
 	public void registerUsers() throws Exception {
-		ppApp.registerUser(u);
+		ppApp.registerUser(user1);
 		assertEquals(1, ppApp.getUsers().size());
 	}
 	
 	@Test
 	public void registerUsers_checkNames() throws Exception {
-		ppApp.registerUser(u);
-		assertEquals(u.getFirstname(), "Andreas");
-		assertEquals(u.getLastname(), "Ustrup");
+		assertEquals(user1.getFirstname(), "John");
+		assertEquals(user1.getLastname(), "Nielsen");
+	}
+	
+	@Test
+	public void registerUsers_checkUserId() throws Exception {
+		ppApp.registerUser(user1);
+		assertEquals(user1.getUserId(), "joni");
+	}
+	
+	@Test
+	public void registerUsers_checkUserId_DuplicateIds() throws Exception {
+		ppApp.registerUser(user1);
+		User user2 = new User("Jonni", "Nissen");
+		ppApp.registerUser(user2);
+		assertEquals(user1.getUserId(), "joni");
+		assertEquals(user2.getUserId(), "jois");
 	}
 
 	@Test
 	public void deregisterUser() throws Exception {
-		ppApp.registerUser(u);
-		ppApp.deregisterUser(u);
+		ppApp.registerUser(user1);
+		ppApp.deregisterUser(user1);
 		assertEquals(0, ppApp.getUsers().size());
 	}
 	
-	@Test
-	public void invalidUserRegistation_UserExists() throws Exception {
-		ppApp.registerUser(u);
-		try {
-			ppApp.registerUser(u);
-			fail("Expected RegistrationException");
-		}
-		catch(RegistrationException e) {
-			assertEquals("UserId already in use", e.getMessage());
-			assertEquals("Register user", e.getOperation());
-		}
-	}
+//	@Test
+//	public void invalidUserRegistation_UserExists() throws Exception {
+//		ppApp.registerUser(user1);
+//		try {
+//			ppApp.registerUser(user1);
+//			fail("Expected RegistrationException");
+//		}
+//		catch(RegistrationException e) {
+//			assertEquals("UserId already in use", e.getMessage());
+//			assertEquals("Register user", e.getOperation());
+//		}
+//	}
 	
 	@Test
 	public void invalidUserDeregistation_UserDoesNotExist() throws Exception {
