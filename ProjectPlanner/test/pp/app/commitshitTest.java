2368dcf0581c2d6899e6b06dd539238233f51659
Pelle Rubin <pelle.galloe@gmail.com>
2016-05-02 14:05:03 +0200

    tilføjede en test af lort

---
+++ ProjectPlanner/test/pp/app/commitshitTest.java
@@ -0,0 +1,14 @@
+package pp.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class commitshitTest {
+
+	@Test
+	public void test() {
+		System.out.println("hej hej");
+	}
+
+}
