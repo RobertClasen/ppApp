87527cea49438f49952e442dbd6b6327cf86c2ea
RobertClasen <s143321@dtu.dk>
2016-05-08 15:41:19 +0200

    Ny skrifttype.

--- ProjectPlanner/src/pp/ui/View.java
+++ ProjectPlanner/src/pp/ui/View.java
@@ -1,19 +1,25 @@
 package pp.ui;
 
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import pp.app.PpApp;
 
 public class View extends BorderPane {
 	private PpApp ppApp;
 	private Controller controller;
-	
+
+	private ImageView imageView;
+	private Image logo;
 	private ScrollPane scrollPane;
 	private Screen text;
 	private InputField inputField;
 
 	public View(PpApp ppApp) {
 		this.ppApp = ppApp;
+		logo = new Image("pp/ui/ppLogo.png");
+		imageView = new ImageView(logo);
 		setId("root");
 		
 		createNodes();
@@ -34,14 +40,10 @@ public class View extends BorderPane {
 			scrollPane.setVvalue(1.0);
 			inputField.clear();
 		});
-		
-//		inputField.addListener((observable) -> {
-//			scrollPane.setVvalue(1.0);
-//			inputField.clear();
-//		});
 	}
 	
 	private void arrangeNodes() {
+		setTop(imageView);
 		setCenter(scrollPane);
 		setBottom(inputField);
 		scrollPane.setContent(text);
